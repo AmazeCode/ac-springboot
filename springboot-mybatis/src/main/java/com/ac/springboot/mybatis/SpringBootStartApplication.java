b7e0dc4da822fed2fec99c6eaa7168f8c1377793
PC-20190616HMDA\Administrator <1558628010@qq.com>
2019-11-18 20:24:54 +0800

    分离Mybatis配置类

--- springboot-mybatis/src/main/java/com/ac/springboot/mybatis/SpringBootStartApplication.java
+++ springboot-mybatis/src/main/java/com/ac/springboot/mybatis/SpringBootStartApplication.java
@@ -12,7 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 // mapper 接口类扫描包配置
-@MapperScan(value = "com.ac.springboot.mybatis.dao")
+//@MapperScan(value = "com.ac.springboot.mybatis.dao")
 public class SpringBootStartApplication {
 
     public static void main(String[] args) {
